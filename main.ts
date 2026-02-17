0e587fc6ab4a5858b978faa7c3e805132818b468
148963-cell <148963@kvilleps.org>
2026-02-17 07:58:06 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ namespace SpriteKind {
     export const NPC = SpriteKind.create()
 }
 
+//alaina
+
 // -------------- DATA STRUCTURES -----------------------
 //creating new types and interfaces is a touch too much 
 //for what I want to cover in this class. 
